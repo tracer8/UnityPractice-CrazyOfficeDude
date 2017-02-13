efe35e03786b2f126b16dea75207b7effb70f2c5
Spie Studio <studio.spie@gmail.com>
2017-02-13 13:02:31 +0700

    - Make player damage Effect

--- Assets/Scripts/Managers/PlayerController.cs
+++ Assets/Scripts/Managers/PlayerController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
@@ -23,7 +24,9 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
-    public Animator deadAnimator;
+    public Image imageDamageEffect;
+
+    private IEnumerator coroutineEffectDamage;
 	
     void Awake()
     {
@@ -32,8 +35,8 @@ public class PlayerController : MonoBehaviour {
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-            StartCoroutine(TakeDamage(20));
+        if (Input.GetKeyDown(KeyCode.Space))
+            StartCoroutine(TakeDamage(10));
     }
 
     private void DisableMeshRender()
@@ -47,8 +50,46 @@ public class PlayerController : MonoBehaviour {
         Health -= damageAmount;
 
         GameManager.Notification.PostNotification(this, "OnPlayerTakeDamage");
+        ShowDamageEffect();
 
         yield return null;
+    }
+
+    private void ShowDamageEffect()
+    {
+        if (coroutineEffectDamage != null)
+            StopCoroutine(coroutineEffectDamage);
+
+        coroutineEffectDamage = Coroutine_DamageEffect();
+        StartCoroutine(coroutineEffectDamage);
+    }
+
+    private IEnumerator Coroutine_DamageEffect()
+    {
+        imageDamageEffect.gameObject.SetActive(true);
+        imageDamageEffect.color = new Color(255, 0, 0, 0.2f);
+
+        float elapsedTime = 0;
+        float fadeOutTime = 1.0f;
+
+        Color newColor = imageDamageEffect.color;
+        float startAlpha = newColor.a;
+
+        while (true)
+        {
+            if (elapsedTime >= fadeOutTime)
+                break;
+
+            newColor.a = Mathf.Lerp(startAlpha, 0, elapsedTime / fadeOutTime);
+
+            imageDamageEffect.color = newColor;
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        imageDamageEffect.gameObject.SetActive(false);
     }
 
     private IEnumerator Die()
@@ -57,10 +98,7 @@ public class PlayerController : MonoBehaviour {
 
         GameManager.Notification.PostNotification(this, "OnPlayerDead");
 
-        if (deadAnimator != null)
-        {
-            deadAnimator.SetTrigger("ShowDeath");
-        }
+        Debug.LogWarning("Player DEAD!!!!!!");
 
         yield return new WaitForSeconds(respawnTime);
 
